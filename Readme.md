2354f2d05367511907df38d54c12322514ce3d20
cvija25 <lcvijicc@gmail.com>
2024-12-17 16:03:44 +0100

    optics refactor

---
+++ Readme.md
@@ -0,0 +1,7 @@
+### Optics in C++
+
+`g++ optics1.cpp`
+
+`./a.out`
+
+`python3 plot.py`
--- optics1.cpp
+++ optics1.cpp
@@ -10,7 +10,6 @@
 #include <algorithm>
 #include <fstream>
 
-
 using namespace std;
 
 const double UNDEFINED = numeric_limits<double>::max();
@@ -25,15 +24,15 @@ struct Point {
 
 double euclideanDistance(const Point& p1, const Point& p2) {
     double dist = 0.0;
-    for (size_t i = 0; i < p1.coords.size(); ++i) {
+    for (int i = 0; i < p1.coords.size(); ++i) {
         dist += pow(p1.coords[i] - p2.coords[i], 2);
     }
     return sqrt(dist);
 }
 
-vector<Point> loadCsv(string filename) {
+vector<Point>* loadCsv(string filename) {
     ifstream file(filename);
-    vector<Point> data;
+    vector<Point>* data = new vector<Point>;
     string line, cell;
     // first line
     getline(file, line);
@@ -44,19 +43,25 @@ vector<Point> loadCsv(string filename) {
         while (getline(lineStream, cell, ',')) {
             row.push_back(stod(cell));
         }
-        data.push_back(Point(row));
+        data->push_back(Point(row));
     }
     return data;
 }
 struct Optics {
-    vector<Point>& points;
-    vector<int>& processingOrder;
+    vector<Point>* m_points;
+    vector<int>* m_processingOrder;
     int epsilon, minPts;
 
-    Optics(int e, int mP, vector<Point>& p, vector<int>& po) : epsilon(e), minPts(mP), points(p), processingOrder(po) {}
+    Optics(int e, int mP) : epsilon(e), minPts(mP), m_points(nullptr), m_processingOrder(nullptr) {}
 
-    vector<int> getNeighbors(const vector<Point>& points, int idx) {
+    ~Optics() {
+        delete m_points;
+        delete m_processingOrder;
+    }
+
+    vector<int> getNeighbors(int idx) {
         vector<int> neighbors;
+        vector<Point>& points = *m_points;
         for (size_t i = 0; i < points.size(); ++i) {
             if (i != idx && euclideanDistance(points[idx], points[i]) <= epsilon) {
                 neighbors.push_back(i);
@@ -65,7 +70,9 @@ struct Optics {
         return neighbors;
     }
 
+    // goes through each unprocessed neighbor and updates it's reachability and puts them in priority queue
     void updateReachability(priority_queue<pair<double, int>, vector<pair<double, int>>, greater<>> &seeds, const vector<int>& neighbors, int idx) {
+        vector<Point>& points = *m_points;
         double coreDist = neighbors.size() >= minPts ? euclideanDistance(points[idx], points[neighbors[minPts - 1]]) : UNDEFINED;
         if (coreDist != UNDEFINED) {
             for (int neighbor : neighbors) {
@@ -80,22 +87,30 @@ struct Optics {
         }
     }
 
-    void fit() {
-        for (size_t i = 0; i < points.size(); ++i) {
+    void fit(vector<Point>& points) {
+        m_points = &points;
+        m_processingOrder = new vector<int>;
+
+        // goes through each point
+        for (int i = 0; i < points.size(); ++i) {
             if (!points[i].processed) {
                 points[i].processed = true;
-                processingOrder.push_back(i);
-                vector<int> neighbors = getNeighbors(points, i);
+                m_processingOrder->push_back(i);
+                vector<int> neighbors = getNeighbors(i);
+
+                // if it's a core point
                 if (neighbors.size() >= minPts) {
                     priority_queue<pair<double, int>, vector<pair<double, int>>, greater<>> seeds;
                     updateReachability(seeds, neighbors, i);
+
+                    // porcesses all "near" points
                     while (!seeds.empty()) {
                         int current = seeds.top().second;
                         seeds.pop();
                         if (!points[current].processed) {
                             points[current].processed = true;
-                            processingOrder.push_back(current);
-                            vector<int> currentNeighbors = getNeighbors(points, current);
+                            m_processingOrder->push_back(current);
+                            vector<int> currentNeighbors = getNeighbors(current);
                             if (currentNeighbors.size() >= minPts) {
                                 updateReachability(seeds, currentNeighbors, current);
                             }
@@ -106,22 +121,22 @@ struct Optics {
         }
     }
 
+    // we define cluster by setting a threshold
     vector<vector<int>> extractClusters(double clusterDistanceThreshold) {
         vector<vector<int>> clusters;
         vector<int> currentCluster;
+        vector<Point>& points = *m_points;
 
-        for (int idx : processingOrder) {
-            if (points[idx].reachabilityDist > clusterDistanceThreshold) {
-                if (!currentCluster.empty()) {
-                    clusters.push_back(currentCluster);
-                    currentCluster.clear();
-                }
-            } else {
+        for (int idx : *m_processingOrder) {
+            if (points[idx].reachabilityDist < clusterDistanceThreshold) {
                 currentCluster.push_back(idx);
+            } else if (!currentCluster.empty()) {
+                clusters.push_back(currentCluster);
+                currentCluster.clear();
             }
         }
 
-        // Add the last cluster if it exists
+        // add the last cluster if it exists
         if (!currentCluster.empty()) {
             clusters.push_back(currentCluster);
         }
@@ -129,12 +144,14 @@ struct Optics {
         return clusters;
     }
 
+    // we export clusers to file so we can plot them in python, TODO in c++
     void exportClusters(const vector<vector<int>>& clusters, const string& filename) {
         ofstream file(filename);
+        // we do 2 dimensions for siplicity
         file << "x,y,cluster\n";
+        vector<Point>& points = *m_points;
 
-        // Assign cluster numbers
-        for (size_t clusterId = 0; clusterId < clusters.size(); ++clusterId) {
+        for (int clusterId = 0; clusterId < clusters.size(); ++clusterId) {
             for (int idx : clusters[clusterId]) {
                 file << points[idx].coords[0] << "," << points[idx].coords[1] << "," << clusterId + 1 << "\n";
             }
@@ -161,56 +178,31 @@ struct Optics {
 
 
 int main() {
-    vector<Point> points = loadCsv("klasterovanje.csv");
+    vector<Point>* points = loadCsv("klasterovanje.csv");
     vector<int> processingOrder;
-    Optics* model = new Optics(3.0, 7, points, processingOrder);
+    Optics* model = new Optics(3.0, 7);
 
 
     double clusterDistanceThreshold = 2.5;
 
-    model->fit();
-
-    cout << "Point\tReachability Distance\n";
-    for (size_t i = 0; i < points.size(); ++i) {
-        cout << i << "\t";
-        if (points[i].reachabilityDist == UNDEFINED)
-            cout << "UNDEFINED\n";
-        else
-            cout << points[i].reachabilityDist << "\n";
-    }
-
-    // Output reachability plot
-    cout << "\nReachability Plot (Processing Order):\n";
-    for (int idx : model->processingOrder) {
-        cout << idx << "\t";
-        if (points[idx].reachabilityDist == UNDEFINED)
-            cout << "UNDEFINED\n";
-        else
-            cout << points[idx].reachabilityDist << "\n";
-    }
+    model->fit(*points);
 
     ofstream outFile("reachability_plot.csv");
     outFile << "Index,ReachabilityDist\n";
-    for (int idx : model->processingOrder) {
+    for (int idx : *model->m_processingOrder) {
         outFile << idx << ",";
-        if (points[idx].reachabilityDist == UNDEFINED)
+        if ((*points)[idx].reachabilityDist == UNDEFINED)
             outFile << "UNDEFINED\n";
         else
-            outFile << points[idx].reachabilityDist << "\n";
+            outFile << (*points)[idx].reachabilityDist << "\n";
     }
     outFile.close();
 
     vector<vector<int>> clusters = model->extractClusters(clusterDistanceThreshold);
 
-    cout << "\nClusters:\n";
-    for (size_t i = 0; i < clusters.size(); ++i) {
-        cout << "Cluster " << i + 1 << ": ";
-        for (int idx : clusters[i]) {
-            cout << idx << " ";
-        }
-        cout << "\n";
-    }
     model->exportClusters(clusters, "clusters.csv");
+
+    delete model;
 
     return 0;
 }
